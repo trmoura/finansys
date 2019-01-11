3ff462f5d07b14187fc6182e66ee3883b86db687
Tiago Moura <trmoura@yahoo.com.br>
2019-01-11 16:13:18 -0200

    improving category list component

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
 const routes: Routes = [
   { path: '', component: CategoryListComponent },
-  { path: 'new', component: CategoryListComponent },
-  { path: ':id/edit', component: CategoryListComponent }
+  { path: 'new', component: CategoryFormComponent },
+  { path: ':id/edit', component: CategoryFormComponent }
 ];
 
 @NgModule({
